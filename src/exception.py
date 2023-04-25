b4cc9a972bc1e6424f272c4628c147ab0958b6f1
manishkr1754 <manishkr1754@gmail.com>
2023-04-25 10:53:24 +0530

    draft EDA & Model Training

--- src/exception.py
+++ src/exception.py
@@ -10,7 +10,6 @@ def error_message_detail(error,error_detail:sys):
 
     return error_message
 
-    
 
 class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
@@ -20,10 +19,3 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
 
-
-if __name__==="__main__":
-    try:
-        a=1/0
-    except Exception as e:
-        logging.info("Divide by Zero")
-        raise CustomException(e,sys)
